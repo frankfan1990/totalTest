14b5029d76b9e3f773e8108bbf1eaa40b6d79cb2
frankfan <769683385@qq.com>
2014-12-17 09:04:53 +0800

    frankfan-12-17 | 9:04

---
+++ JustGitTest/JustGitTest/FrankfanFiveViewController.m
@@ -0,0 +1,37 @@
+//
+//  FrankfanFiveViewController.m
+//  JustGitTest
+//
+//  Created by frankfan on 14/12/17.
+//  Copyright (c) 2014年 Rching. All rights reserved.
+//
+
+#import "FrankfanFiveViewController.h"
+
+@interface FrankfanFiveViewController ()
+
+@end
+
+@implementation FrankfanFiveViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
